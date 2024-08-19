adb2e59bbcabf145a5cf1a125a7e2a67beac4655
TiagoPaulin <thiago.vmp15@gmail.com>
2024-08-19 15:49:32 -0300

    User entity and resource

---
+++ src/main/java/com/tiagopaulin/curso/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.tiagopaulin.curso.resources;
+
+import com.tiagopaulin.curso.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+
+        User u = new User(1L, "Maria", "maria@gmail", "999999", "123456");
+
+        return ResponseEntity.ok().body(u);
+
+    }
+
+}
