244dcb08dd49005da6a3221f6be3ee29c75f297c
TiagoPaulin <thiago.vmp15@gmail.com>
2024-08-20 15:18:18 -0300

    Product implementation

---
+++ src/main/java/com/tiagopaulin/curso/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.tiagopaulin.curso.repositories;
+
+import com.tiagopaulin.curso.entities.Category;
+import com.tiagopaulin.curso.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+
+}
---
+++ src/main/java/com/tiagopaulin/curso/services/ProductService.java
@@ -0,0 +1,31 @@
+package com.tiagopaulin.curso.services;
+
+import com.tiagopaulin.curso.entities.Product;
+import com.tiagopaulin.curso.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> findAll() {
+
+        return productRepository.findAll();
+
+    }
+
+    public Product findById(Long id) {
+
+        Optional<Product> obj = productRepository.findById(id);
+
+        return obj.get();
+
+    }
+
+}
